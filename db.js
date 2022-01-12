9a77a7cc10c73cc4336c61f70a63af0a95b10145
Ky3bK4 <k.kyzmin321@gmail.com>
2022-01-12 13:11:23 +0200

    commit

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const {Sequelize} = require('sequelize')
 
 module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
     dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 )
