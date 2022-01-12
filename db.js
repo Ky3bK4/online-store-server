ab2e7957d5f0139dab179419f1853b938b33d716
Ky3bK4 <k.kyzmin321@gmail.com>
2022-01-12 14:33:03 +0200

    changes

--- db.js
+++ db.js
@@ -1,18 +1,30 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-  // process.env.DB_NAME,
-  // process.env.DB_USER,
-  // process.env.DB_PASSWORD,
-  process.env.DATABASE_URL,
-  {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
+if(process.env.NODE_ENV === 'development') {
+  module.exports = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT
+    }
+  )
+} else if(process.env.NODE_ENV === 'production') {
+  console.log('wew')
+  module.exports = new Sequelize(
+    process.env.DATABASE_URL,
+    {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
       }
     }
-  }
-)
+  )
+}
+
